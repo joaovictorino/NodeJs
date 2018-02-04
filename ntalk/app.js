ec9de726a5bb3df9b7f1c74396b289d0b9d2d586
João Henrique Victorino <jh_vs@hotmail.com>
2018-02-04 01:34:15 -0200

    atualização

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var path = require('path');
-var consign = require('consign');
-var app = express();
+const express = require('express');
+const path = require('path');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set(cookieParser('ntalk'));
+app.set(expressSession());
+app.set(bodyParser.json());
+app.set(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
 consign({})
